3582516355aa1aae7c203d1eb28f8dbe1748d8e5
AnkitaSinha2618 <sinhaankita2618@gmail.com>
2024-09-05 00:07:09 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", 
 {id:'heading', xyz:'abc'}, //to add attributes in H1 tag
 "Hello world from React!");
 
-{/* <div id="parent">
-    <div id="child">
-        <h1>I am h1 tag</h1>
-        <h2>I am h2 tag</h2>
-    </div>
-
-</div> */}
 
 const parent = React.createElement("div",{id:"parent"}, 
     [React.createElement("div", {id:"child"},
     [
-        React.createElement("h1", {}, "I am h1 tag"), 
+        React.createElement("h1", {}, "This is namaste react "), 
         React.createElement("h2", {}, "I am h2 tag")
     ]),
     React.createElement("div", {id:"child"},
