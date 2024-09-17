b72797cbb5cfade7eab984c2d4b9e29cca99282f
AnkitaSinha2618 <sinhaankita2618@gmail.com>
2024-09-17 23:55:13 +0530

    episode 7 part 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,42 @@
 import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   // let btnName = "Login";
-   const [reactBtn, setreactBtn]= useState("Login");
-   console.log("header component render");
-    return (
-
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={LOGO_URL}
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-            <button className="login" onClick={()=>{reactBtn === "Login"?setreactBtn("Logout"):setreactBtn("Login")}}>{reactBtn}</button>
-          </ul>
-        </div>
+  const [reactBtn, setreactBtn] = useState("Login");
+  console.log("header component render");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
       </div>
-    );
-  };
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About Us</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact Us</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              reactBtn === "Login"
+                ? setreactBtn("Logout")
+                : setreactBtn("Login");
+            }}
+          >
+            {reactBtn}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
