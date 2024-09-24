5e2f0f4dbbac62cd5180dbc0881d3cd297e38675
AnkitaSinha2618 <sinhaankita2618@gmail.com>
2024-09-25 00:41:14 +0530

    episode-8 part-6(make api calls and life cycle- CBC)

--- src/components/About.js
+++ src/components/About.js
@@ -5,40 +5,24 @@ import UserClass from "./UserClass";
 class About extends Component{
     constructor(props){
         super(props);
-        console.log("Parent constructor");
-        
+        // console.log("Parent constructor");  
     }
 
     componentDidMount(){
-        console.log("parent componentDidMount");
-        
+        // console.log("parent componentDidMount");     
     }
 
     render(){
-        console.log("Parent render");
-        
+        // console.log("Parent render");
         return(
             <div>
             <h1>About</h1>
             <h2>This is Namaste react</h2>
-            {/* <User name={"Ankita Sinha function"}/> */}
             <UserClass name={"1st class"} location={"Hyderabad class"}/>
-            <UserClass name={"2nd class"} location={"Hyderabad class"}/>
-            <UserClass name={"3rd class"} location={"Hyderabad class"}/>
             </div>  
         )
     }
 }
 
-// const About = () =>{
-//     return(
-//         <div>
-//             <h1>About</h1>
-//             <h2>This is Namaste react</h2>
-//             {/* <User name={"Ankita Sinha function"}/> */}
-//             <UserClass name={"Ankita Sinha class"} location={"Hyderabad class"}/>
-//         </div>
-//     )
-// }
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,32 +3,39 @@ import React from "react";
 class UserClass extends React.Component{
     constructor(props){
         super(props);
-
+        // console.log(this.props.name + "Child Constructor");   
         this.state = {
-            count :0,
-        }
-        console.log(this.props.name + "Child Constructor");
-        
+            userInfo:{
+                name : "Dummy",
+                location : "Default"
+            }
+        } 
     }
 
-    componentDidMount(){
-        console.log(this.props.name+"Child componentDismount");
+    async componentDidMount(){
+        // console.log("parent componentDidMount");  
+        const data = await fetch("https://api.github.com/users/ankitasinha2618");
+        const json = await data.json()
+        this.setState({
+            userInfo : json,
+        })
+        
+        console.log(json);    
+    }
+
+    componentDidUpdate(){
+        console.log("Componet Did Update called")
+    }
+    componentWillUnmount(){
+        console.log("componentWillunmount called");
         
     }
     render(){
-        console.log(this.props.name+"Child render");
-        
-        const {name, location} = this.props;
-        const {count} = this.state;
+        // console.log(this.props.name+"Child render");
+        const {name, location, avatar_url} = this.state.userInfo;
         return(
-            <div className="user-card">
-            <h1>Count:{count}</h1>
-            <button onClick={()=> {
-                // Never update state variable directly
-                this.setState({
-                    count: this.state.count + 1
-                })
-            }}>Count Increased</button>
+        <div className="user-card">
+            <img src={avatar_url} style={{height:"200px"}}/> 
            <h2>Name: {name}</h2>
             <h3>Location: {location}</h3>
             <h4>Contact: @ankita2618</h4>
@@ -37,3 +44,21 @@ class UserClass extends React.Component{
     }
 }
 export default UserClass;
+
+/**
+ * <----Mounting Phase--->
+ * constructor called
+ * render (dummy data)
+ * <html (dummy data)>
+ * componentdidMount called
+ *   <API called>
+ *   <this.state>
+ * 
+ * <----Update Phase--->
+ * render(API data)
+ * <html (api data)>
+ * componentDidUpdate() called
+ * 
+ * <---Unmounting Phase--->
+ * componentWillUnmount() called
+ */
